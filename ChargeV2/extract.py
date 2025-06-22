a45bb8de8c87a8fd70af2e1a60e262de92770c15
Advaith Anand <advaitha259@gmail.com>
2025-06-22 00:47:42 -0700

    finished data extraction using pandas

--- ChargeV2/extract.py
+++ ChargeV2/extract.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def getactivetime():
 
 def getinactivetime():
@@ -8,3 +9,19 @@ def check_extracted():
 def extract_all():
     # extract all of the important information from the battery report
     # this includes the 
+def gettable(file_path):
+    history = pd.read_html(file_path)
+    history = history[2]
+    history = history[history['STATE'] != 'Suspended']
+    history = history.drop(["CAPACITY REMAINING.1", "STATE"], axis = 1)
+    map = {
+        "AC": 1,
+        "Battery": 0
+    }
+    history["SOURCE"] = history["SOURCE"].map(map)
+    history["CAPACITY REMAINING"] = history["CAPACITY REMAINING"].str[:-2].astype(float)
+    dt_full = pd.to_datetime(history['START TIME'], errors = "coerce")
+    history["DATE"] = dt_full.dt.date.ffill()
+    history['TIME'] = pd.to_datetime(history['START TIME'], format = "mixed").dt.time
+    history = history.drop("START TIME", axis = 1)
+    return history
