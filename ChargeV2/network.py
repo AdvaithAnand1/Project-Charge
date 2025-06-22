0e2550fba9885070b3468ca92d0230bafe202af7
Advaith Anand <advaitha259@gmail.com>
2025-06-22 12:19:47 -0700

    final network designed

--- ChargeV2/network.py
+++ ChargeV2/network.py
@@ -4,18 +4,22 @@ def save():
 
 def trainnetwork(table):
     class chargenet(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = torch.nn.Sequential(
-            torch.nn.Linear(3, 16),
-            torch.nn.ReLU(),
-            torch.nn.Linear(16, 8),
-            torch.nn.ReLU(),
-            torch.nn.Linear(8, 2)
-        )
-    def forward(self, x):
-        return self.net(x)
+        def __init__(self):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.Linear(3, 16),
+                torch.nn.ReLU(),
+                torch.nn.Linear(16, 8),
+                torch.nn.ReLU(),
+                torch.nn.Linear(8, 2)
+            )
+        def forward(self, x):
+            return self.net(x)
     model = chargenet()
     lossfn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.0005)
     
+    history = extract.gettable(r"C:\Windows\System32\battery-report.html")
+
+    X = torch.from_numpy(history.drop("SOURCE", axis = 1).astype("float32").values)
+    Y = torch.from_numpy(history["SOURCE"].astype("int64").values)
